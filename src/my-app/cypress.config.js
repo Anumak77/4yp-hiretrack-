67c8f7e3810c2e9dbe72061fb776932fea08f21e
Anushree Umak <anushree.umak2@mail.dcu.ie>
2025-03-09 03:25:37 +0000

    cypres 18
    Former-commit-id: 46dbbc6b68356e6712875c151ee05a101e4d2d28

--- src/my-app/cypress.config.js
+++ src/my-app/cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    // Look for any files ending in `.cy.js` inside `my-app/cypress/e2e/`
     specPattern: 'cypress/e2e/**/*.cy.js', 
     baseUrl: 'http://localhost:3000',
     supportFile: false
