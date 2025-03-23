eed419dc863b8a9f18dff7854a367bb5e6f95bcd
Anushree Umak <anushree.umak2@mail.dcu.ie>
2025-03-23 15:29:44 +0000

    updated ci/cd
    Former-commit-id: 49558cee962547ce8f5a22fd49c10a13c87cb8bd

--- src/my-app/cypress.config.js
+++ src/my-app/cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // you can leave this empty
-    },
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    setupNodeEvents(on, config) {
+      // optional
+    },
   },
 });
